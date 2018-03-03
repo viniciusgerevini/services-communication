d6acd5766d960b48f0706fa3400e6c3b7713d390
Vinicius Gerevini <viniciusgerevini@gmail.com>
2018-03-03 19:24:53 +1100

    implemented bus parser

--- sydney-realtime-transport/src/gtfs-response-parser.js
+++ sydney-realtime-transport/src/gtfs-response-parser.js
@@ -1,3 +1,16 @@
 module.exports = function feedResponseParser(decodeGtfsMessage) {
+  return function parseGtfsMessage(body) {
+    const feed = decodeGtfsMessage(body);
+    return {
+      id: feed.id,
+      type: 'bus',
+      trip_id: feed.vehicle.trip.trip_id,
+      route_id: feed.vehicle.trip.route_id,
+      position: {
+        latitude: feed.vehicle.position.latitude,
+        longitude: feed.vehicle.position.longitude
+      }
+    };
+  };
 };
 
---
+++ sydney-realtime-transport/src/gtfs-response-parser.test.js
@@ -0,0 +1,38 @@
+import test from 'ava';
+import gtfsParser from './gtfs-response-parser';
+
+test('parse bus object', (t) => {
+  const fakeDecoder = message => message;
+
+  const parser = gtfsParser(fakeDecoder);
+
+  const feedObject = {
+    id: 'someid',
+    vehicle: {
+      trip: {
+        trip_id: '577478',
+        route_id: '2433_T75'
+      },
+      position: {
+        latitude: -33.74142074584961,
+        longitude: 150.91769409179688,
+        bearing: 185,
+        odometer: null,
+        speed: 14.800000190734863
+      }
+    }
+  };
+
+  const expected = {
+    id: feedObject.id,
+    type: 'bus',
+    trip_id: feedObject.vehicle.trip.trip_id,
+    route_id: feedObject.vehicle.trip.route_id,
+    position: {
+      latitude: feedObject.vehicle.position.latitude,
+      longitude: feedObject.vehicle.position.longitude
+    }
+  };
+
+  t.deepEqual(parser(feedObject), expected);
+});
